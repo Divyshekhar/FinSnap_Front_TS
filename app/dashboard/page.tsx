f15e9b57e3f6892814705af06c485fcc090ce815
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-02-24 01:48:31 +0530

    adding token removal after expiration

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,31 @@
 "use client"
+import { Box, Typography } from "@mui/material";
+import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/navigation";
 import React from "react";
+
+type JwtPayload = {
+    userId: string,
+    email: string,
+    exp: number
+}
 
 export default function Dashboard(){
     const router = useRouter();
     const [isLoading, setisLoading] = React.useState(true);
+    const isTokenExpired = (token: string | null) => {
+        if (!token) return true;
+        try {
+            const decoded: JwtPayload = jwtDecode<JwtPayload>(token);
+            return decoded.exp * 1000 < Date.now();
+        } catch (error) {
+            return true;
+        }
+    };
     React.useEffect(()=>{
         const token = localStorage.getItem('authToken');
-        if(!token){
+        if(!token || isTokenExpired(token)){
+            localStorage.removeItem("authToken");
             router.push('/signin');
         }
         else{
@@ -16,8 +34,10 @@ export default function Dashboard(){
     }, [router])
     if(isLoading) return <p>Loading...</p>
     return(
-        <div style={{color: "white"}}>
-            Hi from dashboard
-        </div>
+        <Box>
+            <Typography sx={{color: "white"}}>
+                Hi this is from dashboard
+            </Typography>
+        </Box>
     )
 }
