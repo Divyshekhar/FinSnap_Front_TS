b7afba22df665998b8ff276a047e1291b3ffe9c3
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-02-24 02:10:13 +0530

    adding a protect-layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,41 +3,17 @@ import { Box, Typography } from "@mui/material";
 import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Protected from "../protected-layout";
 
-type JwtPayload = {
-    userId: string,
-    email: string,
-    exp: number
-}
 
-export default function Dashboard(){
-    const router = useRouter();
-    const [isLoading, setisLoading] = React.useState(true);
-    const isTokenExpired = (token: string | null) => {
-        if (!token) return true;
-        try {
-            const decoded: JwtPayload = jwtDecode<JwtPayload>(token);
-            return decoded.exp * 1000 < Date.now();
-        } catch (error) {
-            return true;
-        }
-    };
-    React.useEffect(()=>{
-        const token = localStorage.getItem('authToken');
-        if(!token || isTokenExpired(token)){
-            localStorage.removeItem("authToken");
-            router.push('/signin');
-        }
-        else{
-            setisLoading(false);
-        }
-    }, [router])
-    if(isLoading) return <p>Loading...</p>
-    return(
-        <Box>
-            <Typography sx={{color: "white"}}>
-                Hi this is from dashboard
-            </Typography>
-        </Box>
+export default function Dashboard() {
+    return (
+        <Protected>
+            <Box>
+                <Typography sx={{ color: "white" }}>
+                    Hi this is from dashboard
+                </Typography>
+            </Box>
+        </Protected>
     )
 }
---
+++ app/protected-layout.tsx
@@ -0,0 +1,27 @@
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
+import { useEffect, ReactNode } from "react";
+
+type JwtPayload = {
+    userId: string,
+    email: string,
+    iat: number,
+    exp: number
+};
+
+export default function Protected({children}: {children: ReactNode}){
+    const router = useRouter();
+    useEffect(()=>{
+        const token = localStorage.getItem('authToken');
+        if(!token){
+            router.push('/signin');
+            return;
+        }
+        const decoded: JwtPayload = jwtDecode<JwtPayload>(token);
+        if(decoded.exp * 1000 < Date.now()){
+            localStorage.removeItem('authToken');
+            router.push('/signin');
+        }
+    }, [router])
+    return<>{children}</>
+}
